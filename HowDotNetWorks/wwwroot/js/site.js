6d543b5a3500bd87ad69a7426a8dd8cf4437be91
Ben Holland <bhollan5@kent.edu>
2022-03-17 13:50:28 -0400

    Adding spa page loading

--- HowDotNetWorks/wwwroot/js/site.js
+++ HowDotNetWorks/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+window.onload = function () {
+    console.log(`Now loading ${window.location.pathname}`);
+    let path = window.location.pathname.split('/')[1];
+    let menuLinks = document.getElementsByClassName("menu-link");
+    for (let i = 0; i < menuLinks.length; i++) {
+        let menuLink = menuLinks[i];
+        if (menuLink.id == path) {
+            menuLink.classList.add('selected-link');
+        } else {
+            menuLink.classList.remove('selected-link');
+        }
+    }
+}
