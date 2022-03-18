087671316ce1fc5df99eaaf3b15db0343f65f2f3
Ben Holland <bhollan5@kent.edu>
2022-03-17 22:15:31 -0400

    Adding page content

---
+++ HowDotNetWorks/Pages/ServingADotNetWebsite.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HowDotNetWorks.Pages
+{
+    public class ServingADotNetWebsiteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
